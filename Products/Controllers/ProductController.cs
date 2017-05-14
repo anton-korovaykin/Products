5a057a621c5369e7d04d295e80c123e4e8b3cff4
Anton Korovaykin <anton.korovaykin@arvosoftware.com>
2017-05-14 11:28:14 +0300

    Add API

---
+++ Products/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Products.Models;
+
+namespace Products.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var context = new DAL.Products();
+            return context.GetAll();
+
+        }
+
+        [System.Web.Http.HttpGet]
+        public Product Get(int id)
+        {
+            var context = new DAL.Products();
+            return context.GetById(id);
+        }
+
+        [System.Web.Http.HttpPost]
+        public Product Post(Product product)
+        {
+            var context = new DAL.Products();
+            return context.Add(product);
+        }
+
+        [System.Web.Http.HttpPut]
+        public Product Put(Product product)
+        {
+            var context = new DAL.Products();
+            return context.Update(product);
+        }
+
+        [System.Web.Http.HttpDelete]
+        public void Delete(int id)
+        {
+            var context = new DAL.Products();
+            context.Delete(id);
+        }
+    }
+}
--- Products/Global.asax.cs
+++ Products/Global.asax.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +18,10 @@ namespace Products
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
